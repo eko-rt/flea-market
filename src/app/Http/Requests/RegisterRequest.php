9d94aff839c46ed42a05f29a624eaa47c11611fb
eko-rt <x.s2.w.any@gmail.com>
2025-05-10 23:08:32 +0900

    会員登録画面のバリデーション

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -33,7 +33,9 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name.required' => 'お名前を入力してください。',
+            'name.max' => 'お名前は255文字以内で入力してください。',
             'email.required' => 'メールアドレスを入力してください。',
+            'email.email' => '正しい形式でメールアドレスを入力してください。',
             'password.required' => 'パスワードを入力してください。',
             'password.min' => 'パスワードは8文字以上で入力してください。',
             'password.confirmed' => 'パスワードと一致しません。',
